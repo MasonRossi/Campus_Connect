9e4b501b2aef66a63580dbe60bde5dbd75392aac
MasonRossi <Mrossi0770@conestogac.on.ca>
2026-02-07 02:09:53 -0500

    Big commit

--- CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
+++ CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3091fac55632a4aa33a5e36a4b5da5d37038fe8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0fe1f10bd5eb8e8d067eeac0b38272ba54e95d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
