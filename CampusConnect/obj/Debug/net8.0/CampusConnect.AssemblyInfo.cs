407ed338b6940ad23fedd531488afde321a37f57
MasonRossi <Mrossi0770@conestogac.on.ca>
2026-02-22 17:55:21 -0500

    create page Access denied fix

--- CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
+++ CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d600472404c0e1885eb59f419509c7f3be72712")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b1d895a5788179776ba363ea7f2b3108913afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
