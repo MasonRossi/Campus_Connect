f78629aa08ba6bcc97dc22739c14c8e26029c568
MasonRossi <Mrossi0770@conestogac.on.ca>
2026-02-22 15:24:51 -0500

    update Commit

--- CampusConnect/obj/Release/net8.0/CampusConnect.AssemblyInfo.cs
+++ CampusConnect/obj/Release/net8.0/CampusConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0fe1f10bd5eb8e8d067eeac0b38272ba54e95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4b501b2aef66a63580dbe60bde5dbd75392aac")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
