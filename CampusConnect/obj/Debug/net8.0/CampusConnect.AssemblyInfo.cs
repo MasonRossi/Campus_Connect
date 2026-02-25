364ab0be65d27e3dcc2c62276c79265363b86cc0
MasonRossi <Mrossi0770@conestogac.on.ca>
2026-02-24 23:00:52 -0500

    cleanup

--- CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
+++ CampusConnect/obj/Debug/net8.0/CampusConnect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b896a3f08ece2ff491c2fb8851e2eaf1f68f88df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d65539cd2ca31283fc9abbb9f93965d9257c801")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
